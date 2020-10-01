e0e902b0d4e1a42d70ee45058674a920f6ce588d
Sebastian Pietras <seba00767@gmail.com>
2020-10-02 00:04:32 +0200

    Added getting dlib predictor path as context manager

--- automakeup/automakeup/__init__.py
+++ automakeup/automakeup/__init__.py
@@ -1,1 +1,9 @@
-dlib_predictor_path = "dlib_face_predictor_68.dat"
+import importlib.resources as pkg_resources
+
+
+def _get_resource(name):
+    return pkg_resources.path("automakeup.resources", name)
+
+
+def dlib_predictor_path():
+    return _get_resource("dlib_face_predictor_68.dat")
